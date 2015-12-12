bbbb339cc83f91d48317ceed3d0ffcd6e1c1e57e
Howell Leung <632063469@qq.com>
2015-12-12 23:52:02 +0800

    Initial Commit

---
+++ TaskMoment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaskMoment
+//
+//  Created by 梁浩 on 15/12/12.
+//  Copyright © 2015年 LeungHowell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
